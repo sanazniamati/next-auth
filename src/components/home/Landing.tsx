ea0f91833faf785e087580f0e36fbfd322108a7a
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2024-06-22 17:08:33 +0330

    added landing

---
+++ src/components/home/Landing.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import Menu from "../menu/Menu";
+import { Main } from "./style";
+
+function Landing() {
+  return (
+    <Main>
+      <Menu />
+      <span className="headerText">همه از یک مبلمان خوب استقبال می کنند</span>
+    </Main>
+  );
+}
+
+export default Landing;
---
+++ src/components/home/style.ts
@@ -0,0 +1,20 @@
+/** @format */
+
+"use client";
+
+import styled from "styled-components";
+export const Main = styled.div`
+  width: 100%;
+  height: 900px;
+  background-image: url("assets/images/header.jpg");
+  background-repeat: no-repeat;
+  position: relative;
+  .headerText {
+    position: absolute;
+    font-size: 80px;
+    font-family: "YekanBakh Heavy";
+    color: #ffffff;
+    top: 283px;
+    right: 117px;
+  }
+`;
