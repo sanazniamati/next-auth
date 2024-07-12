60b3f7ec2c74a501b8b0637637442aa28f7c47bb
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2024-07-12 21:16:18 +0330

    added custom Ant config

---
+++ src/components/providers/AntConfig.ts
@@ -0,0 +1,15 @@
+/** @format */
+
+import { OverrideToken } from "antd/es/theme/interface";
+
+export const AntConfigProvider: OverrideToken = {
+  Button: {
+    colorPrimary: "red",
+    // algorithm: true,
+    fontSize: 16,
+  },
+  Input: {
+    colorPrimary: "red",
+    // algorithm: true,
+  },
+};
